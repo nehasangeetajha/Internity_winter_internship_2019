1eee06820d92ab638f89d5eed724613687539643
Sangeeta Jha <43818333+nehasangeetajha@users.noreply.github.com>
2020-01-03 13:38:26 +0530

    Update quicksort.cpp

--- team1/assignment-2/Quick sort/quicksort.cpp
+++ team1/assignment-2/Quick sort/quicksort.cpp
@@ -9,35 +9,6 @@ void swap(T* a, T* b)
     *b = t;
 }
 
-template<class T>
-int partition (T arr[], int low, int high)
-{
-    T pivot = arr[high];
-    int i = (low - 1);
-
-    for (int j = low; j <= high - 1; j++)
-    {
-        if (arr[j] < pivot)
-        {
-            i++;
-            swap(&arr[i], &arr[j]);
-        }
-    }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
-}
-
-template<class T>
-void quickSort(T arr[], int low, int high)
-{
-    if (low < high)
-    {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
-}
-
 template <class T>
 void printArray(T arr[], int n)
 {
@@ -45,6 +16,44 @@ void printArray(T arr[], int n)
     for (i = 0; i < n; i++)
         cout << arr[i] << " ";
     cout << endl;
+}
+
+
+template <class T>
+int Partition(T arr[],int low,int high)
+{
+    int left,right;
+    T key = arr[low];
+    left = low;
+    right= high;
+
+    while(left<right)
+    {
+        while(arr[left]<=key)
+            left++;
+        while(arr[right]>key)
+            right--;
+        if(left<right)
+        {
+            swap(&arr[left],&arr[right]);
+        }
+    }
+    arr[low] = arr[right];
+    arr[right] = key;
+    return right;
+}
+
+template <class T>
+void quickSort(T arr[],int low,int high)
+{
+    int pivot;
+    //Base case
+    if(high>low)
+    {
+        pivot = Partition(arr,low,high);
+        quickSort(arr,low,pivot-1);
+        quickSort(arr,pivot+1,high);
+    }
 }
 
 int main()
